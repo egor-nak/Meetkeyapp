196cdff8cda48550acd55b1f837f2b3de79aaacb
Egornak <egor.nak@bk.ru>
2021-05-21 16:51:01 +0300

    Работает стартовое окно

--- appcoverage/mainapp.py
+++ appcoverage/mainapp.py
@@ -1,66 +1,140 @@
 import time
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((500, 600))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 20)
 
+# класс который отвечает за кнопку начала
+class Startbutton(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        height = int(H * 0.1)
+        width = int(W * 0.24)
 
-class Button:
-    """Create a button, then blit the surface in the while loop"""
+        # создаём поверхность для кнопки чтобы она была как спрайт
+        self.image = pygame.Surface((width, height))
+        self.image.fill((255, 255, 255))
 
-    def __init__(self, text, pos, font, bg="black", feedback="bye"):
-        self.x, self.y = pos
-        self.font = pygame.font.SysFont("Arial", font)
-        if feedback == "":
-            self.feedback = "text"
-        else:
-            self.feedback = feedback
-        self.change_text(text, bg)
+        # рисуем на этой поверхности текст и кнопку !!!! учти что когда мы рисуем на поверхности
+        # кординаты учитываются относительно аоверхности а не относительно всего экрана !!!!!
+        pygame.draw.rect(self.image, (0, 179, 255), [0, 0, width, height], border_radius=50)
+        # рисуем текст
+        font = pygame.font.Font(None, 36)
+        text = font.render('Start', True, (255, 255, 255))
+        self.recttext = text.get_rect()
+        self.recttext.x = width // 2 - (self.recttext.width // 2)
+        self.recttext.y = height // 2 - (self.recttext.height // 2)
+        self.image.blit(text, self.recttext)
+        self.rect = self.image.get_rect()
 
-    def change_text(self, text, bg="black"):
-        """Change the text whe you click"""
-        self.text = self.font.render(text, 1, pygame.Color("White"))
-        self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
-        self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+        self.rect.x = x
+        self.rect.y = y
+        self.x = x
+        self.y = y
 
     def show(self):
-        screen.blit(button.surface, (self.x, self.y))
+        self.rect.x = self.x
+        self.rect.y = self.y
 
-    def click(self, event):
-        x, y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
-                    self.change_text(self.feedback, bg="red")
+    def hide(self):
+        self.rect.x = -1000
+        self.rect.y = -1000
+
+    #  проверка нажатия
+    def click_detection(self, x, y):
+        if x in range(self.rect.x, self.rect.x + self.rect.width) and y in range(self.rect.y,
+                                                                                 self.rect.y + self.rect.height):
+            return True
+        else:
+            return False
+
+
+# класс который отвечает за логотип
+class Logo(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(
+            'images/logo.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.x = x
+        self.y = y
+
+    def show(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def hide(self):
+        self.rect.x = -10000
+
+
+class Motto(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((1000, 50))
+        self.image.fill((255, 255, 255))
+        font = pygame.font.Font(None, 48)
+        text = font.render('We are like a Swiss knife for any online meeting', False, (0, 0, 0))
+        self.recttext = text.get_rect()
+        self.recttext.x = 200
+        self.recttext.y = 0
+        self.image.blit(text, self.recttext)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.x = x
+        self.y = y
+
+    def show(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def hide(self):
+        self.rect.x = -100000
 
 
 if __name__ == '__main__':
     pygame.init()
-
+    sprite = []
+    sprite_group = pygame.sprite.Group()
     screen = pygame.display.set_mode()
+    W, H = screen.get_size()
     size = width, height = screen.get_size()
     running = True
-    v = 20
     fps = 60
     clock = pygame.time.Clock()
-    # button = Button("hi", (200, 200), 100)
-    flag = False
+
+    # создаём все спрайты
+    start = Startbutton(W // 2 - int(W * 0.12), H - int(H * 0.3))
+    logowidth = 300
+    logoheight = 200
+    logo = Logo(W // 2 - int(logowidth * 0.4), logoheight * 0.6, logowidth, logoheight)
+    motto = Motto(W // 2 - int(560), H - int(H * 0.4))
+
+    # добавляем все спрайты в группу чтобы с ними было удобнее работать
+    sprite.append(start)
+    sprite_group.add(start)
+    sprite.append(logo)
+    sprite_group.add(logo)
+    sprite.append(motto)
+    sprite_group.add(motto)
+
     while running:
         screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                flag = True
-                coords = event.pos
-                size = 20
-                pygame.draw.circle(screen, (255, 255, 0), coords, size)
+                pos = pygame.mouse.get_pos()
+                if start.click_detection(pos[0], pos[1]):
+                    start.hide()
+                    logo.hide()
+                    motto.hide()
             if event.type == pygame.QUIT:
                 running = False
-        # button.show()
         clock.tick(fps)
+        sprite_group.update()
+        sprite_group.draw(screen)
+        for sp in sprite:
+            screen.blit(sp.image, sp.rect)
+        pygame.display.update()
         pygame.display.flip()
     pygame.quit()
